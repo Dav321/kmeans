1fc9f13d880f036c2c69bfc5a2668cc054675337
David Schmidt <64831818+Dav321@users.noreply.github.com>
2026-03-20 18:48:20 +0100

    implement KMeans UI with stepping

---
+++ src/kmeans.rs
@@ -0,0 +1,154 @@
+use ggez::{Context, GameError};
+use ggez::event::EventHandler;
+use ggez::glam::Vec2;
+use ggez::graphics::{Canvas, Color, DrawMode, DrawParam, Mesh, MeshBuilder, MeshData, Rect, StrokeOptions, Text};
+use ggez::input::keyboard::{Key, KeyInput};
+use ggez::input::mouse::MouseButton;
+use ggez::winit::keyboard::{NamedKey, PhysicalKey};
+use rand::{rng, RngExt};
+
+pub struct KMeans {
+    points: Vec<(f32, f32)>,
+    ks: Vec<(f32, f32)>,
+}
+
+impl KMeans {
+    pub fn new(_ctx: &mut Context) -> Self {
+        Self {
+            points: Vec::new(),
+            ks: Vec::new(),
+        }
+    }
+
+    pub fn random(&mut self, points: u32, ks: u32) {
+        let mut rng = rng();
+
+        for _ in 0..points {
+            let coords = (rng.random_range(0.0..1.0), rng.random_range(0.0..1.0));
+            self.points.push(coords);
+        }
+
+        for _ in 0..ks {
+            let coords = (rng.random_range(0.0..1.0), rng.random_range(0.0..1.0));
+            self.ks.push(coords);
+        }
+    }
+
+    fn distance2(p1: &(f32, f32), p2: &(f32, f32)) -> f32 {
+        let x = p1.0 - p2.0;
+        let y = p1.1 - p2.1;
+
+        x * x + y * y
+    }
+
+    fn nearest_k(&self, point: &(f32, f32)) -> usize {
+        let mut smallest_index = 0;
+        let mut smallest_val = 2.;
+
+        let mut i = 0;
+        for k in self.ks.iter() {
+            let d = Self::distance2(point, k);
+            if d < smallest_val {
+                smallest_index = i;
+                smallest_val = d;
+            }
+            i+=1;
+        }
+        smallest_index
+    }
+
+    fn iterate(&mut self) {
+        let mut k_sum: Vec<(f32, f32, f32)> = vec![(0., 0., 0.); self.ks.len()];
+        for point in self.points.iter() {
+            let k = self.nearest_k(&point);
+            let (x, y, n) = k_sum[k];
+            k_sum[k] = (
+                x + point.0,
+                y + point.1,
+                n + 1.
+            );
+        }
+
+        for i in 0..self.ks.len() {
+            let (x, y, n) = k_sum[i];
+            let x = x / n;
+            let y = y / n;
+
+            self.ks[i] = (x, y);
+        }
+    }
+
+    fn build_mesh(&self, ctx: &mut Context, size: f32) -> Mesh {
+        const COLORS: [Color; 6] = [Color::GREEN, Color::BLUE, Color::RED, Color::CYAN, Color::MAGENTA, Color::YELLOW];
+        let mut mb = MeshBuilder::new();
+
+        mb.rectangle(
+            DrawMode::stroke(10.),
+            Rect::new(5., 5., size - 5., size - 5.),
+            Color::WHITE
+        ).expect("Failed to draw outline");
+        let size = size - 10.;
+
+        for i in 0..self.points.len() {
+            let point = self.points[i];
+            let k_i = self.nearest_k(&point);
+
+            mb.circle(
+                DrawMode::stroke(1.0),
+                Vec2::new(point.0 * size, point.1 * size),
+                size * 0.005,
+                1.0,
+                COLORS[k_i]
+            ).expect("Failed to paint Point");
+        }
+
+        for i in 0..self.ks.len() {
+            let point = self.ks[i];
+            mb.circle(
+                DrawMode::fill(),
+                Vec2::new(point.0 * size, point.1 * size),
+                size * 0.005,
+                1.0,
+                COLORS[i]
+            ).expect("Failed to paint Point");
+        }
+
+        Mesh::from_data(ctx, mb.build())
+    }
+}
+
+impl EventHandler for KMeans {
+    fn update(&mut self, ctx: &mut Context) -> Result<(), GameError> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> Result<(), GameError> {
+        let mut canvas = Canvas::from_frame(ctx, Color::BLACK);
+        let (w, h) = ctx.gfx.size();
+        let size = w.min(h);
+
+        let fps_text = format!("FPS: {:.0}", ctx.time.fps());
+        canvas.draw(
+            Text::new(fps_text)
+                .set_scale(48.),
+            Vec2::new(10., 10.)
+        );
+
+        let mesh = self.build_mesh(ctx, size);
+        canvas.draw(&mesh, DrawParam::default());
+
+        canvas.finish(ctx)
+    }
+
+    fn key_down_event(&mut self, ctx: &mut Context, input: KeyInput, _repeated: bool) -> Result<(), GameError> {
+        match input.event.logical_key {
+            Key::Named(NamedKey::Enter) => self.iterate(),
+            _ => {}
+        }
+        Ok(())
+    }
+
+    fn mouse_button_down_event(&mut self, _ctx: &mut Context, _button: MouseButton, _x: f32, _y: f32) -> Result<(), GameError> {
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod kmeans;
+
+use ggez::conf::{Conf, FullscreenType};
+use ggez::ContextBuilder;
+use ggez::event::run;
+use crate::kmeans::KMeans;
+
 fn main() {
-    println!("Hello, world!");
+    let mut conf = Conf::new();
+    conf.window_setup.title = "KMeans".to_string();
+    conf.window_mode.fullscreen_type = FullscreenType::Desktop;
+    let (mut ctx, event_loop) = ContextBuilder::new("kmeans", "David Schmidt")
+        .default_conf(conf)
+        .build()
+        .expect("Could not create Context!");
+    let mut kmeans = KMeans::new(&mut ctx);
+    kmeans.random(100, 6);
+    run(ctx, event_loop, kmeans);
 }
